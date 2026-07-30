05ecea2516f407fd3aa33b67e687fe15dde2f37a
Aditya Raj Pal <6063.adityarajpal@gmail.com>
2026-07-30 12:02:15 +0530

    Time: 0 ms (100%), Space: 8.2 MB (95.83%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int minimumPushes(string word) {
         int s=word.size();
-        int q=s/8;
-        int i=1;
-        int r=s%8;
-        int count=0;
+        // int q=s/8;
+        // int i=1;
+        // int r=s%8;
+        // int count=0;
         // for(i;i<=q;i++){
         //     count=count+i*8;
         // }
-        count=q*(q+1)*4+(q+1)*r;
-        return count;
+        // count=q*(q+1)*4+(q+1)*r;
+        return (s/8)*((s/8)+1)*4+((s/8)+1)*(s%8);
     }
 };
