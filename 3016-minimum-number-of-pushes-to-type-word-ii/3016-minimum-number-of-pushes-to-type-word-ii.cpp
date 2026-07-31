3c1419df06945374f8f7bd604f6174bd15beca99
Aditya Raj Pal <6063.adityarajpal@gmail.com>
2026-07-31 11:56:30 +0530

    Time: 11 ms (48.07%), Space: 28.5 MB (15.83%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -8,17 +8,17 @@ public:
         sort(freq.rbegin(),freq.rend());
         int cost=0;
         int p=1;
-        int temp=1;
-        // for(auto x:freq){
-        //     cost+=x*p;
-        //     temp++;
-        //     if(temp%8==0){
-        //         p++;
-        //     }
-        // }
-        for(int i = 0; i < 26; i++){
-    cost += freq[i] * (i / 8 + 1);
-}
+        int temp=0;
+        for(auto x:freq){
+            cost+=x*p;
+            temp++;
+            if(temp%8==0){
+                p++;
+            }
+        }
+//         for(int i = 0; i < 26; i++){
+//     cost += freq[i] * (i / 8 + 1);
+// }
         return cost;
 
 
