4d319f182e13555bf60b2931a83d75c870689cb0
Aditya Raj Pal <6063.adityarajpal@gmail.com>
2026-08-06 09:59:36 +0530

    Time: 4 ms (0.71%), Space: 9.4 MB (3.33%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -1,26 +1,37 @@
 class Solution {
 public:
-    int product(int n){
-        int product1=1;
-        while(n>0){
-            int x=n%10;
-            product1=product1*x;
-            n=n/10; 
-        }
-        return product1;
-    }
+    // int product(int n){
+    //     int product1=1;
+    //     while(n>0){
+    //         int x=n%10;
+    //         product1=product1*x;
+    //         n=n/10; 
+    //     }
+    //     return product1;
+    // }
     int smallestNumber(int n, int t) {
-        int y=n;
+        // int y=n;
         
-        while(true){
-            if(product(y)%t==0)
-            return y;
+        // while(true){
+        //     if(product(y)%t==0)
+        //     return y;
     
-            y++;
+        //     y++;
         
         
+        // }
+        // return y;
+        while(true){
+            string a=to_string(n);
+            int product=1;
+            for(int i=0;i<a.size();i++){
+                product*=int(a[i]-'0');
+            }
+            if(product%t==0)
+            return n;
+            n++;
         }
-        return y;
+        return n;
         
     }
 };
