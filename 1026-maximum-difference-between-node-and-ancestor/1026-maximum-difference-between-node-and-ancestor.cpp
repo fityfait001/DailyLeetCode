8876d48ef6883defc8aad654e5ceab17d23269e3
Aditya Raj Pal <6063.adityarajpal@gmail.com>
2026-08-30 23:59:37 +0530

    Time: 0 ms (100%), Space: 13.8 MB (20.42%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
@@ -9,27 +9,49 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     int maxDiff=-1;
+//     void findMaxUtil(TreeNode * root,TreeNode *child){
+//         if(root==NULL || child==NULL){
+//             return;
+//         }
+//         maxDiff=max(maxDiff,abs(root->val-child->val));
+//         findMaxUtil(root,child->left);
+//         findMaxUtil(root,child->right);
+//     }
+//     int maxAncestorDiff(TreeNode* root){
+//         if(root==NULL){
+//             return maxDiff;
+//         }
+//         findMaxUtil(root,root->left);
+//         findMaxUtil(root,root->right);
+
+//         maxAncestorDiff(root->left);
+//         maxAncestorDiff(root->right);
+//         return maxDiff;
+        
+//     }
+// };
+
 class Solution {
 public:
-    int maxDiff=-1;
-    void findMaxUtil(TreeNode * root,TreeNode *child){
-        if(root==NULL || child==NULL){
-            return;
+    int minv=INT_MAX;
+    int maxv=-1;
+    int findMaxDiff(TreeNode* root,int minv,int maxv){
+        if(root==NULL){
+            return abs(maxv-minv);
         }
-        maxDiff=max(maxDiff,abs(root->val-child->val));
-        findMaxUtil(root,child->left);
-        findMaxUtil(root,child->right);
+        minv=min(minv,root->val);
+        maxv=max(maxv,root->val);
+
+        int l=findMaxDiff(root->left,minv,maxv);
+        int r=findMaxDiff(root->right,minv,maxv);
+        
+        return max(l,r);
     }
     int maxAncestorDiff(TreeNode* root){
-        if(root==NULL){
-            return maxDiff;
-        }
-        findMaxUtil(root,root->left);
-        findMaxUtil(root,root->right);
-
-        maxAncestorDiff(root->left);
-        maxAncestorDiff(root->right);
-        return maxDiff;
+        return findMaxDiff(root,root->val,root->val);
         
     }
 };
