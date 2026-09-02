9476c528263957f5ce5dd22c0a3bfbe65528b720
Aditya Raj Pal <6063.adityarajpal@gmail.com>
2026-09-02 18:42:49 +0530

    Time: 3 ms (31.12%), Space: 13.8 MB (12.71%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> powerset;
+        vector<int>partial;
+        int i=0;
+        dfs(nums,i,partial,powerset);
+        return powerset;
+    }
+private:
+    void dfs(vector<int> &nums,int i,vector<int>&partial,vector<vector<int>>&powerset){
+        if(i==nums.size()) powerset.push_back(partial);
+        else{
+            int val=nums[i];
+            int k=1;
+            while(i+k<nums.size()&& nums[i+k]==val) k++;
+            dfs(nums,i+k,partial,powerset);
+            partial.push_back(nums[i]);
+            dfs(nums,i+1,partial,powerset);
+            partial.pop_back();
+        }
+    }
+};
