3f69b4ba079e8862094ff96093abeffbda3ffcbb
Aditya Raj Pal <6063.adityarajpal@gmail.com>
2026-09-21 00:02:38 +0530

    Time: 41 ms (6.46%), Space: 16.4 MB (43.2%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     string sortVowels(string s) {
-        
+        vector<char> v;
+
+        for (char c : s) {
+            if (string("aeiouAEIOU").find(c) != string::npos)
+                v.push_back(c);
+        }
+
+        sort(v.begin(), v.end());
+
+        int j = 0;
+        for (char &c : s) {
+            if (string("aeiouAEIOU").find(c) != string::npos)
+                c = v[j++];
+        }
+
+        return s;
     }
 };
