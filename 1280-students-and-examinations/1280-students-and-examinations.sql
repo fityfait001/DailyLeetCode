4ba1983f48b434514ee72f9db1a3f8e93d728451
Aditya Raj Pal <6063.adityarajpal@gmail.com>
2026-09-21 19:05:53 +0530

    Time: 1270 ms (28.37%), Space: 0B (100%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -1,9 +1,27 @@
-# Write your MySQL query statement below
-select s.student_id,s.student_name,sub.subject_name,
-count(e.subject_name) as attended_exams 
-from students s
-cross join subjects sub
-left join examinations e 
-on s.student_id=e.student_id and sub.subject_name=e.subject_name
-group by s.student_id,s.student_name,sub.subject_name
-order by s.student_id,sub.subject_name;
+-- Input: 
+-- Students table:
+-- +------------+--------------+
+-- | student_id | student_name |
+-- +------------+--------------+
+-- | 1          | Alice        |
+-- | 2          | Bob          |
+-- | 13         | John         |
+-- | 6          | Alex         |
+-- +------------+--------------+
+-- Subjects table:
+-- +--------------+
+-- | subject_name |
+-- +--------------+
+-- | Math         |
+-- | Physics      |
+-- | Programming  |
+-- +--------------+
+select s.student_id,s.student_name,sb.subject_name,
+ifnull(count(e.subject_name), 0) as attended_exams 
+from students s 
+cross join subjects sb
+left join examinations e
+on s.student_id=e.student_id
+and e.subject_name=sb.subject_name
+group by s.student_id,s.student_name,sb.subject_name
+order by s.student_id,sb.subject_name;
