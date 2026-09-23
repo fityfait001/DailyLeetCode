ddaf92d5cce214bd5428355c2e2d18eb0ef7ccf8
Aditya Raj Pal <6063.adityarajpal@gmail.com>
2026-09-23 10:39:09 +0530

    Time: 307 ms (67.72%), Space: 0B (100%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
-select name ,population ,area 
-from world where
-(area>=3000000 or population >=25000000);
+SELECT NAME,POPULATION,AREA
+FROM WORLD
+WHERE AREA>=3000000 OR
+POPULATION >=25000000
