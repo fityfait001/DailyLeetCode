7f1c01081d36f448aa1d8984ea8157d8eec599bc
Aditya Raj Pal <6063.adityarajpal@gmail.com>
2026-09-24 18:10:44 +0530

    Time: 0 ms (100%), Space: 10.2 MB (97.19%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        vector<vector<int>> powerset;
-        vector<int>partial;
-        int i=0;
-        dfs(nums,i,partial,powerset);
-        return powerset;
-    }
-private:
-    void dfs(vector<int> &nums,int i,vector<int>&partial,vector<vector<int>>&powerset){
-        if(i==nums.size()) powerset.push_back(partial);
-        else{
-            int val=nums[i];
-            int k=1;
-            while(i+k<nums.size()&& nums[i+k]==val) k++;
-            dfs(nums,i+k,partial,powerset);
-            partial.push_back(nums[i]);
-            dfs(nums,i+1,partial,powerset);
-            partial.pop_back();
+    void findSubset(int i,vector<int>&ds,vector<vector<int>>&ans,vector<int>& nums){
+        ans.push_back(ds);
+        for(int j=i;j<nums.size();j++){
+            if(j!=i && nums[j]==nums[j-1]){
+                continue;
+            }
+            ds.push_back(nums[j]);
+            findSubset(j+1,ds,ans,nums);
+            ds.pop_back();
         }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int>ds;
+        sort(nums.begin(),nums.end());
+        findSubset(0,ds,ans,nums);
+        return ans;
     }
 };
